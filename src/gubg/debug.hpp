17e3e1996c5de660e57b477e4e9051b8b5594dee
geert <geert.fannes@auro-technologies.com>
2016-02-16 11:51:09 +0100

    added basic non-thread-safe scoping

--- src/gubg/debug.hpp
+++ src/gubg/debug.hpp
@@ -4,13 +4,51 @@
 #include "gubg/macro/stream.hpp"
 #include <iostream>
 
-#define S(debug_ns) \
-    const char * const l_debug_ns = debug_ns; \
-    if (!!l_debug_ns) {std::cout << ">>" << l_debug_ns << std::endl;}
+namespace gubg { namespace debug { 
+
+    namespace details { 
+        template <typename T>
+            struct Level { static unsigned int value; };
+        template <typename T>
+            unsigned int Level<T>::value = 0;
+    } 
+
+    class Scope
+    {
+        public:
+            Scope(std::ostream &os, const char *debug_ns, const char *func): os_(os), debug_ns_(debug_ns), func_(func)
+            {
+                if (do_log())
+                {
+                    stream() << ">> " << debug_ns_ << " => " << function_name() << std::endl;
+                    ++details::Level<Scope>::value;
+                }
+            }
+            ~Scope()
+            {
+                if (do_log())
+                {
+                    --details::Level<Scope>::value;
+                    stream() << "<< " << debug_ns_ << " => " << function_name() << std::endl;
+                }
+            }
+
+            bool do_log() const {return !!debug_ns_;}
+            std::ostream &stream() {return os_ << std::string(2*details::Level<Scope>::value, ' ');}
+            const char *function_name() const {return !!func_ ? func_ : "<no function name given>";}
+
+        private:
+            std::ostream &os_;
+            const char *debug_ns_;
+            const char *func_;
+    };
+} } 
+
+#define S(debug_ns) gubg::debug::Scope l_gubg_debug_Scope(std::cout, debug_ns, __func__)
 
 #define L(msg) do { \
-    if (!!l_debug_ns) \
-        std::cout << l_debug_ns << "::" << __func__ << ": " << msg << " (" << __FILE__ << ':' << __LINE__ << ")" << std::endl; \
+    if (l_gubg_debug_Scope.do_log()) \
+        l_gubg_debug_Scope.stream() << "   " << msg << std::endl; \
     } while (false)
 
 #endif
