b478663aa61af3e26016350a874d869f3808e192
Geert Fannes <geert.fannes@gmail.com>
2016-04-30 17:49:56 +0200

    added start of Range.hpp

---
+++ src/gubg/Range.hpp
@@ -0,0 +1,12 @@
+#ifndef HEADER_gubg_Range_hpp_ALREADY_INCLUDED
+#define HEADER_gubg_Range_hpp_ALREADY_INCLUDED
+
+#ifdef RANGE
+#error RANGE already defined
+#endif
+#define RANGE(ctr) (ctr).begin(), (ctr).end()
+
+namespace gubg { 
+} 
+
+#endif
