375d04a99a791138d3e5df6e55cf7dee4238c56d
Geert Fannes <geert.fannes@auro-technologies.com>
2016-07-15 08:30:50 +0200

    added type support for C()

--- src/gubg/macro/capture.hpp
+++ src/gubg/macro/capture.hpp
@@ -1,10 +1,14 @@
 #ifndef HEADER_gubg_macro_capture_hpp_ALREADY_INCLUDED
 #define HEADER_gubg_macro_capture_hpp_ALREADY_INCLUDED
 
+#include "gubg/macro/variadic.h"
+
 #ifdef C
 #error C already defined
 #endif
 
-#define C(var) "{" #var ":" << var << "}"
+#define C_1(var) "{" #var ":" << var << "}"
+#define C_2(var, type) "{" #var ":" << type(var) << "}"
+#define C(...) GUBG_GET_ARG_3((__VA_ARGS__, C_2,C_1))(__VA_ARGS__)
 
 #endif
