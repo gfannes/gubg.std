08b7da8262dac35178eec9094b6920977a1696e8
Geert Fannes <geert.fannes@auro-technologies.com>
2017-07-10 00:08:08 +0200

    Capture macro C() now logs in tree attribute style

--- src/gubg/macro/capture.hpp
+++ src/gubg/macro/capture.hpp
@@ -7,8 +7,8 @@
 #error C already defined
 #endif
 
-#define C_1(var) "{" #var ":" << var << "}"
-#define C_2(var, type) "{" #var ":" << type(var) << "}"
+#define C_1(var) "(" #var ":" << var << ")"
+#define C_2(var, type) "(" #var ":" << type(var) << ")"
 #define C(...) GUBG_GET_ARG_3((__VA_ARGS__, C_2,C_1))(__VA_ARGS__)
 
 #endif
