8a7d6fdd7bc8136015faf46332fa89955b99aba6
Geert Fannes <geert.fannes@auro-technologies.com>
2017-07-13 23:38:15 +0200

    Added context to gubg::Range

--- src/gubg/Range.hpp
+++ src/gubg/Range.hpp
@@ -9,33 +9,49 @@
 
 namespace gubg { 
 
+    namespace details { 
+        struct DoNothing { };
+    } 
+
+    template <typename It, typename Context = details::DoNothing>
+    class Range
+    {
+    public:
+        using reference = typename std::iterator_traits<It>::value_type;
+
+        Range(It b, It e): begin_(b), end_(e) {}
+        Range(It b, It e, Context &&context): context_(std::move(context)), begin_(b), end_(e) {}
+
+        bool empty() const {return begin_ == end_;}
+        size_t size() const {return end_ - begin_;}
+        reference front() {return *begin_;}
+
+        It begin() {return begin_;}
+        It end() {return end_;}
+
+        void pop_front() {++begin_;}
+
+    private:
+        Context context_;
+        It begin_;
+        It end_;
+    };
+
     template <typename It>
-        class Range
-        {
-            public:
-                using reference = typename std::iterator_traits<It>::value_type;
-
-                Range(It b, It e): begin_(b), end_(e) {}
-
-                bool empty() const {return begin_ == end_;}
-                size_t size() const {return end_ - begin_;}
-                reference front() {return *begin_;}
-
-                It begin() {return begin_;}
-                It end() {return end_;}
-
-                void pop_front() {++begin_;}
-
-            private:
-                It begin_;
-                It end_;
-        };
-
+    Range<It> make_range(It b, It e)
+    {
+        return Range<It>(b, e);
+    }
+    template <typename It, typename Context>
+    Range<It, Context> make_range(It b, It e, Context &&context)
+    {
+        return Range<It, Context>(b, e, std::move(context));
+    }
     template <typename It>
-        Range<It> make_range(It b, It e)
-        {
-            return Range<It>(b, e);
-        }
+    Range<It> make_range(It b, size_t nr)
+    {
+        return Range<It>(b, b+nr);
+    }
 } 
 
 #endif
---
+++ test/src/gubg/Range_tests.cpp
@@ -0,0 +1,39 @@
+#include "catch.hpp"
+#include "gubg/Range.hpp"
+#include <iostream>
+#include <memory>
+using namespace gubg;
+using namespace std;
+
+TEST_CASE("gubg::Range tests with default context", "[ut][gubg][Range]")
+{
+    const size_t nr = 3;
+    int ary[nr] = {1,2,3};
+
+    auto r = make_range(ary, nr); 
+
+    size_t i = 0;
+    for (auto e: r)
+    {
+        REQUIRE(++i == e);
+        cout << e << ' ';
+    }
+    cout << endl;
+}
+
+TEST_CASE("gubg::Range tests with non-default context", "[ut][gubg][Range]")
+{
+    auto ptr = make_shared<vector<int>>(3);
+    *ptr = {1,2,3};
+    auto r = make_range(ptr->begin(), ptr->end(), std::move(ptr));
+    ptr.reset();
+    auto rr = r;
+
+    size_t i = 0;
+    for (auto e: rr)
+    {
+        REQUIRE(++i == e);
+        cout << e << ' ';
+    }
+    cout << endl;
+}
