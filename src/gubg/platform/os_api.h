f376421174a83acba56e580f218b997804c2b825
Geert Fannes <geert.fannes@gmail.com>
2018-06-26 11:05:45 +0200

    The platform os defines now have value 1

--- src/gubg/platform/os_api.h
+++ src/gubg/platform/os_api.h
@@ -3,20 +3,20 @@
 
 //OS API info
 #ifdef __linux
-#define GUBG_API_LINUX
-#define GUBG_API_POSIX
+#define GUBG_API_LINUX 1
+#define GUBG_API_POSIX 1
 #endif//__linux
 
 #ifdef __APPLE__
-#define GUBG_API_APPLE
-#define GUBG_API_POSIX
+#define GUBG_API_APPLE 1
+#define GUBG_API_POSIX 1
 #endif//__APPLE__
 
 #ifdef _WIN32
-#define GUBG_API_WIN32
+#define GUBG_API_WIN32 1
 #ifdef __MINGW32__
-#define GUBG_API_MINGW
-#define GUBG_API_POSIX
+#define GUBG_API_MINGW 1
+#define GUBG_API_POSIX 1
 #endif//__MINGW32__
 #endif//_WIN32
 
