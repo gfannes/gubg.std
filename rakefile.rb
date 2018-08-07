7988321d1d97cf808b1717edd4adb040ae00d29f
Geert Fannes <geert.fannes@gmail.com>
2018-08-07 07:49:41 +0200

    [prepare] will now always update catch

--- rakefile.rb
+++ rakefile.rb
@@ -1,8 +1,6 @@
 task :prepare do
     Dir.chdir("extern") do
-        unless File.exist?("catch/README.md")
-            sh "git submodule update --init catch"
-        end
+        sh "git submodule update --init catch"
     end
 end
 
