f3d66ca1385c9a7e6f3c368a3d556cf3c6eb088f
Geert Fannes <geert.fannes@auro-technologies.com>
2018-09-17 09:51:48 +0200

    [added] proper task

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,10 @@
+require_relative("../gubg.build/bootstrap.rb")
+require("gubg/shared")
+
 task :prepare do
-    sh "git submodule update --init extern/catch2"
+    Dir.chdir(GUBG.mkdir("extern")) do
+        sh "git clone https://github.com/catchorg/catch2" unless File.exist?("catch2/single_include/catch2/catch.hpp")
+    end
 end
 
 task :run
