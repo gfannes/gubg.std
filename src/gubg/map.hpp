f0d36bc371a64eb7e32ca343087cd8942fef1956
Geert Fannes <geert.fannes@gmail.com>
2019-06-09 00:02:46 +0200

    Renamed get_or() into value_or() => more consistent with std::optional<>

--- src/gubg/map.hpp
+++ src/gubg/map.hpp
@@ -7,7 +7,7 @@
 namespace gubg { 
 
     template <typename T, typename Map, typename Key>
-    T get_or(const T &default_value, const Map &map, const Key &key)
+    T value_or(const T &default_value, const Map &map, const Key &key)
     {
         const auto it = map.find(key);
         if (it == map.end())
