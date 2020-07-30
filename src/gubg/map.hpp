3a9ede8097135887fc6733316a65cb030126cd10
Geert Fannes <geert.fannes@gmail.com>
2020-07-31 01:01:32 +0200

    Added with_value from gubg.io

--- src/gubg/map.hpp
+++ src/gubg/map.hpp
@@ -20,6 +20,14 @@ namespace gubg {
         return res;
     }
 
+    template <typename Map, typename Key, typename Ftor>
+    void with_value(const Map &map, const Key &key, Ftor &&ftor)
+    {
+        auto it = map.find(key);
+        if (it != map.end())
+            ftor(it->second);
+    }
+
 } 
 
 #endif
