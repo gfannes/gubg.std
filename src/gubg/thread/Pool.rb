3885fdbc3aaf51bd3408957636fca30f6b784c6e
Geert Fannes <geert.fannes@gmail.com>
2022-01-21 18:02:12 +0100

    Added start of thread::Pool

---
+++ src/gubg/thread/Pool.rb
@@ -0,0 +1,20 @@
+module GUBG
+    module Thread
+        class Pool
+            def initialize(j: nil)
+            end
+
+            def close()
+            end
+
+            def self.open(j: nil)
+                tp = Pool.new(j: j)
+                yield(tp)
+                tp.close()
+            end
+
+            def add_job()
+            end
+        end
+    end
+end
