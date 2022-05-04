9b3e858a4d30f5ade0621d5656a7046fb58e22fc
Geert Fannes <geert.fannes@gmail.com>
2022-05-04 17:01:42 +0200

    wip

--- rake.rb
+++ rake.rb
@@ -1,5 +1,9 @@
+here = File.dirname(__FILE__)
+
 namespace :std do
 	task :prepare do
-		sh "git submodule update --init --recursive"
+		Dir.chdir(here) do
+			sh "git submodule update --init --recursive"
+		end
 	end
 end
